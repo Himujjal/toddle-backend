e02516ab64e4aa97cd382488573b1b508475a5f8
Himujjal <upadhyayahimujjal@gmail.com>
2020-01-23 15:03:36 +0530

    trying again

--- src/middleware/authMiddleware.ts
+++ src/middleware/authMiddleware.ts
@@ -11,18 +11,20 @@ const protectedRoute: RequestHandler = async (
   res: Response,
   next: NextFunction,
 ) => {
-  const token = req.headers.authorization.split(' ')[1];
+  if (req.headers.authorization) {
+    const token = req.headers.authorization.split(' ')[1];
 
-  jwt.verify(token, process.env.JWT_KEY, async (error, decoded) => {
-    if (error) return res.status(500).send({ error: error.message });
+    jwt.verify(token, process.env.JWT_KEY, async (error, decoded) => {
+      if (error) return res.status(500).send({ error: error.message });
 
-    const user = decoded as IUserFullInfo;
+      const user = decoded as IUserFullInfo;
 
-    req.user = user;
+      req.user = user;
 
-    if (user) next();
-    else res.status(401).json({ error: `Auth failed` });
-  });
+      if (user) next();
+    });
+  }
+  return res.status(401).json({ error: 'Auth failed' });
 };
 
 export default protectedRoute;
